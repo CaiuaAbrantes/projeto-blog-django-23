0147a0a3734dc105e69321b3389519e92500efa1
Caiua <caiua777@gmail.com>
2025-11-24 15:46:10 -0300

    criando model Page e registrando no admin

--- blog/models.py
+++ blog/models.py
@@ -17,3 +17,44 @@ class Tag(models.Model):
         if not self.slug:
             self.slug= slugify_new(self.name)
         return super().save(*args,**kwargs)
+    def __str__(self) -> str:
+        return self.name
+
+
+
+
+
+class Category(models.Model):
+    class meta:
+        verbose_name='Category'
+        verbose_name_plural='Categories'
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(
+        unique=True, default=None, null=True, blank=True
+    )
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify_new(self.name)
+        return super().save(*args, **kwargs)
+    def __str__(self) -> str:
+        return self.name
+
+
+
+class Page(models.Model):
+    title = models.CharField(max_length= 65)
+    slug = models.SlugField(
+        unique=True, default="", null=False, blank=True, max_length=255
+    )
+    is_published = models.BooleanField(
+        default=False,
+        help_text=('Este campo precisara ser marcado' 
+                   'para a pagina ser exbida publicamente')
+                                       )
+    content = models.TextField(null=True, blank=True)
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify_new(self.title)
+        return super().save(*args, **kwargs)
+    def __str__(self) -> str:
+        return self.title
