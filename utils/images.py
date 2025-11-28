5c4fb00d870f0c7fa0eae3b01fc7536118be9e75
Caiua <caiua777@gmail.com>
2025-11-28 15:26:10 -0300

    Filtrando e ordenando Posts enviados para o template view do Django

--- utils/images.py
+++ utils/images.py
@@ -1,11 +1,10 @@
 from pathlib import Path
-
 from django.conf import settings
 from PIL import Image
 
 
 def resize_image(image_django, new_width=800, optimize=True, quality=60):
-    image_path = Path(settings.MEDIA_ROOT / image_django.name).resolve()
+    image_path = Path(settings.MEDIA_ROOT) / image_django.name
     image_pillow = Image.open(image_path)
     original_width, original_height = image_pillow.size
 
@@ -15,7 +14,7 @@ def resize_image(image_django, new_width=800, optimize=True, quality=60):
 
     new_height = round(new_width * original_height / original_width)
 
-    new_image = image_pillow.resize((new_width, new_height))
+    new_image = image_pillow.resize((new_width, new_height), Image.LANCZOS)  # type: ignore[attr-defined]
 
     new_image.save(
         image_path,
